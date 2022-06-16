a3cd008dd02eee89b8c5642b9057341cccec2683
lucas <lucascanellasdev@gmail.com>
2022-06-16 19:06:30 -0300

    att middleware

--- server.js
+++ server.js
@@ -14,9 +14,14 @@ const { uploadFile, getFileStream } = require("./s3")
 
 const app = express()
 
-app.use(cors());
+app.use(cors())
 
-
+// Enable CORS
+/* app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  }); */
 
 app.get('/images/:key', (req, res) => {
     const key = req.params.key
