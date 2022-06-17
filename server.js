367f898d5cc79397eeed21d46e77d97fd9e90207
lucas <lucascanellasdev@gmail.com>
2022-06-16 21:36:39 -0300

    att

--- server.js
+++ server.js
@@ -1,39 +1,59 @@
-const express = require("express")
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 
-const fs = require("fs")
-const util = require("util")
-const unlinkFile = util.promisify(fs.unlink)
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
 
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require("multer");
+const upload = multer({
+  dest: "uploads/",
+  limits: { fileSize: 2 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+        'image/jpeg',
+        'image/pjpeg',
+        'image/png',
+        'image/gif'
+    ]
 
-const port = process.env.PORT || 8080
+    if(allowedMimes.includes(file.mimetype)) {
+        cb(null, true)
+    } else {
+        cb(new Error("Invalid file type."), true)
+    }
+  }
+});
 
-const { uploadFile, getFileStream } = require("./s3")
+const port = process.env.PORT || 8080;
 
-const app = express()
+const { uploadFile, getFileStream } = require("./s3");
+
+const app = express();
 
 app.use(cors())
 
 // Enable CORS
-/* app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  }); */
+/* app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+}); */
 
-app.get('/images/:key', (req, res) => {
-    const key = req.params.key
-    const readStream = getFileStream(key)
-    readStream.pipe(res)
-})
+app.get("/images/:key", (req, res) => {
+  const key = req.params.key;
+  const readStream = getFileStream(key);
+  readStream.pipe(res);
+});
 
-app.post("/images", upload.single("image") , async (req, res) => {
-    const file = req.file
-    const result = await uploadFile(file)
-    await unlinkFile(file.path)
-    res.send({imagePath: `/images/${result.Key}`})
-})
+app.post("/images", upload.single("image"), async (req, res) => {
+  const file = req.file;
+  const result = await uploadFile(file);
+  await unlinkFile(file.path);
+  res.send({ imagePath: `/images/${result.Key}` });
+});
 
-app.listen(port, () => console.log("listening on port 8080"))
+app.listen(port, () => console.log("listening on port 8080"));
